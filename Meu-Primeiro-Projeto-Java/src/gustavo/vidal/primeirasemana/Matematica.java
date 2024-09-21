9dfe3a64cb08960511302a826d0a040ce1013cba
GustavoVidal777 <gustavo.vidal777@yahoo.com.br>
2024-09-21 00:31:40 +0000

    Operadores

--- Meu-Primeiro-Projeto-Java/src/gustavo/vidal/primeirasemana/Matematica.java
+++ Meu-Primeiro-Projeto-Java/src/gustavo/vidal/primeirasemana/Matematica.java
@@ -3,25 +3,22 @@ public class Matematica {
     public static void main(String[] args) {
         int numero1 = 2;
         int numero2 = 8;
-        int total = 0;
+        int contador = 0;
         int x = 0;
 
             for (int i = 0; i < 100; i++) {
                 numero1 = numero1 *1;
                 numero2 = numero2 -1;
-                total = numero1 + numero2;  
+                contador = numero1 + numero2;  
                 x = x +1;
 
-            if (total == 0)               {
+            if (contador == 0 || contador == 5)               {
                 i = 100;
-                System.out.println("Contador chegou a " + total);
+                System.out.println("Contador chegou a " + contador);
                 System.out.println("Numero de operações = " + x);
             }
                 
             }
         }
-
-        
-
     }
 
---
+++ Meu-Primeiro-Projeto-Java/src/gustavo/vidal/primeirasemana/Outro.java
@@ -0,0 +1,17 @@
+package gustavo.vidal.primeirasemana;
+
+public class Outro {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+
+        if (x && y){
+            System.out.println("As duas são corretas");
+        }
+
+        if (x || y){
+            System.out.println("Uma das duas são corretas");
+
+    }
+}
+}
